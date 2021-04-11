7702dd11467a0597c02622511b23bf432ad3997f
Evgeny Blagodarev <y_blagodarev@1cgs.net>
2021-04-11 13:41:04 +0300

    Websockets for vue

--- public/app.js
+++ public/app.js
@@ -1,15 +1,43 @@
 Vue.component("go-app", {
+    data: () => {
+        return {
+            isConnected: false,
+            disconnectedCode: 0
+        }
+    },
+
+    webSockets: {
+        connected() {
+            this.isConnected = true;
+        },
+        disconnected(code) {
+            this.isConnected = false;
+            this.disconnectedCode = code
+        },
+
+        command: {
+
+        }
+    },
+
     template: `
     <v-app>
-      <v-app-bar app>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
-      </v-app-bar>
-      <v-main>  
-        <v-container>
-            hello
+        <v-container v-if="!isConnected">
+            <v-alert
+            border="left"
+            type="info"
+            >Connecting...</v-alert>
+            <v-alert v-if="disconnectedCode"
+            border="left"
+            type="warning"
+            >Error code: {{disconnectedCode}}</v-alert>
         </v-container>
-      </v-main>
+        <v-main v-if="isConnected">  
+            <v-app-bar app>
+                <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+                <v-toolbar-title>Application</v-toolbar-title>
+            </v-app-bar>
+        </v-main>
     </v-app>
     `
 })
---
+++ public/vue-ws.js
@@ -0,0 +1,100 @@
+VueWS = (function() {
+
+const VueWS = {
+        install(Vue, connectionUrl) {
+                const wsConnection = WsConnection(connectionUrl)
+                Vue.prototype.$wsocket = wsConnection.wsocket
+
+                Vue.mixin({
+                        created,
+                        beforeDestroy
+                })
+
+                function created() {
+                        wsConnection.listen(this)
+                }
+                
+                function beforeDestroy() {
+                        wsConnection.unlisten(this)
+                }
+        }
+}
+
+return VueWS
+
+function WsConnection(url) {
+        let isConnected = false
+        const listenedComponents = {}
+
+        const ws = new WebSocket(url)
+        ws.onopen = function() {
+                isConnected = true
+
+                for (component of Object.values(listenedComponents)) {
+                        const connected = component.$options.webSockets.connected;
+                        if (null != connected) {
+                                connected.call(component)
+                        }
+                }
+        }
+        ws.onerror = function(event) {
+                isConnected = false
+        }
+        ws.onclose = function(event) {
+                isConnected = false
+                for (component of Object.values(listenedComponents)) {
+                        const disconnected = component.$options.webSockets.disconnected;
+                        if (null != disconnected) {
+                                disconnected.call(component, event.code)
+                        }
+                }
+        }
+        ws.onmessage = function(event) {
+                const msg = JSON.parse(event.data)
+                const command = msg.command
+                for (component of Object.values(listenedComponents)) {
+                        const wsOptions = component.$options.webSockets
+                        if (wsOptions.command) {
+                                const reciever = wsOptions.command[command]
+                                if (reciever) {
+                                        reciever.call(component, msg)
+                                }
+                        }
+                }
+        }
+
+        function sendMessage(message) {
+                if (!isConnected) {
+                        throw new Error("Ws not ready")
+                }
+                ws.send(JSON.stringify(message))
+        }
+
+        const wsocket = {
+                send: sendMessage
+        }
+
+        function listen(component) {
+                const wsOptions = component.$options.webSockets
+                if (null == wsOptions) {
+                        return
+                }
+                listenedComponents[component._uid] = component
+                
+                if (isConnected && wsOptions.connected) {
+                        wsOptions.connected.call(component)
+                }
+        }
+
+        function unlisten(component) {
+                delete listenedComponents[component._uid]
+        }
+
+        return {
+                wsocket,
+                listen,
+                unlisten
+        }
+}
+
+})()

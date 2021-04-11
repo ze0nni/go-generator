73364173dda3ecbe110a656c94ef2892066e9a23
Evgeny Blagodarev <y_blagodarev@1cgs.net>
2021-04-11 13:52:41 +0300

    Connection to ws

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.7.1 // indirect
 	github.com/gobuffalo/packr/v2 v2.8.1 // indirect
 	github.com/google/uuid v1.2.0
+	github.com/gorilla/websocket v1.4.2
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- webserver/server.go
+++ webserver/server.go
@@ -1,9 +1,11 @@
 package webserver
 
 import (
+	"log"
 	"text/template"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 func Apply(router *gin.Engine) error {
@@ -27,5 +29,15 @@ func (s *server) IndexPage(c *gin.Context) {
 }
 
 func (s *server) WSConnect(c *gin.Context) {
-
+	conn, err := websocket.Upgrade(c.Writer, c.Request, nil, 1024, 1024)
+	if err != nil {
+		log.Printf("Error upgrade request: %s", err)
+	}
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
 }
